fcf7c18461eee9e2e125e1fbeeb12c9ad5fb72aa
alessandrothehuman <132381124+alessandrothehuman@users.noreply.github.com>
2023-05-17 12:40:07 +0200

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-console.log("Hi! Welcome to My Portfiolio Site");
-
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
     x.className += ' responsive';
 } else {
     x.className = 'navtoggle';
